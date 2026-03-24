a8d1199b9aa4feecc03d9d1ab4ee5f4ea12f2b88
Darren Lin <54636352+darrenlingit@users.noreply.github.com>
2026-03-23 21:37:39 -0700

    Update directory

--- directory/make_profiles.R
+++ directory/make_profiles.R
@@ -8,7 +8,7 @@ library(here)
 here::i_am("directory/make_profiles.R")
 
 ##### get environment ready to connect to google drive #####
-download_files <- FALSE # set to FALSE if you don't want to download each time you run this script
+download_files <- TRUE # set to FALSE if you don't want to download each time you run this script
 dotenv::load_dot_env(file = here(".env"))
 spreadsheet_url <- Sys.getenv("SPREADSHEET_URL")
 options(gargle_oauth_cache = ".secrets",
